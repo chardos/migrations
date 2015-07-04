cc6d0601db3cf6b8cafed97d202fe38ec287065e
Victor Augusteo <augusteo@gmail.com>
2015-07-04 19:00:49 +1000

    worldstate and worldaus
    aaaaa
    
    Signed-off-by: Victor Augusteo <augusteo@gmail.com>

---
+++ js/worldauspath.js
@@ -0,0 +1,42 @@
+var path1996 = [
+    {
+      origin: { "latitude": ctry.ita[0], "longitude": ctry.ita[1] },
+      destination: { "latitude": ctry.aus[0], "longitude": ctry.aus[1] },
+      options: { strokeWidth: 10, strokeColor: "rgba(255,0,0,0.5)"}
+    },
+    {
+      origin: { "latitude": ctry.vnm[0], "longitude": ctry.vnm[1] },
+      destination: { "latitude": ctry.aus[0], "longitude": ctry.aus[1] },
+      options: { strokeWidth: 9, strokeColor: "rgba(0,255,0,0.5)"}
+    },
+    {
+      origin: { "latitude": ctry.grc[0], "longitude": ctry.grc[1] },
+      destination: { "latitude": ctry.aus[0], "longitude": ctry.aus[1] },
+      options: { strokeWidth: 8, strokeColor: "rgba(0,0,255,0.5)"}
+    },
+    {
+      origin: { "latitude": ctry.deu[0], "longitude": ctry.deu[1] },
+      destination: { "latitude": ctry.aus[0], "longitude": ctry.aus[1] },
+      options: { strokeWidth: 7, strokeColor: "rgba(255,0,0,0.5)"}
+    },
+    {
+      origin: { "latitude": ctry.chn[0], "longitude": ctry.chn[1] },
+      destination: { "latitude": ctry.aus[0], "longitude": ctry.aus[1] },
+      options: { strokeWidth: 6, strokeColor: "rgba(255,0,0,0.5)"}
+    },
+    {
+      origin: { "latitude": ctry.phl[0], "longitude": ctry.phl[1] },
+      destination: { "latitude": ctry.aus[0], "longitude": ctry.aus[1] },
+      options: { strokeWidth: 5, strokeColor: "rgba(255,0,0,0.5)"}
+    },
+    {
+      origin: { "latitude": ctry.ner[0], "longitude": ctry.ner[1] },
+      destination: { "latitude": ctry.aus[0], "longitude": ctry.aus[1] },
+      options: { strokeWidth: 4, strokeColor: "rgba(255,0,0,0.5)"}
+    },
+    {
+      origin: { "latitude": ctry.ind[0], "longitude": ctry.ind[1] },
+      destination: { "latitude": ctry.aus[0], "longitude": ctry.aus[1] },
+      options: { strokeWidth: 3, strokeColor: "rgba(255,0,0,0.5)"}
+    },
+];

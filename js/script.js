99945af7894bf09a0828a468eb3d0b271324f7e2
Richard Tan <chardos@gmail.com>
2015-07-04 10:38:05 +1000

    added popups

--- js/script.js
+++ js/script.js
@@ -5,130 +5,149 @@ W.vars = {
 	
 };
 
-W.initializeDOMVars = function(){
-
-}
 
 W.init = function() {
-
-
 }
-	
+
+
+//CONFIG
+var arcWidth = 3;
+var arcColor = null;
 
 
 $(document).ready(function() {
 
-       var election = new Datamap({
-        scope: 'world',
-        element: document.getElementById('map'),
-        projection: 'mercator'
-      });
+  var migrationMap = new Datamap({
+    scope: 'world',
+    element: document.getElementById('map'),
+    projection: 'mercator',
 
 
-    var presidentialTrips = [
-        {
-            origin: {
-                latitude: ctry.au[0],
-                longitude: ctry.au[1]
-            },
-            destination: {
-                latitude: 32.066667,
-                longitude: 34.783333 
-            }
-        },
-        {
-            origin: {
-                latitude: ctry.au[0],
-                longitude: ctry.au[1]
-            },
-            destination: {
-                latitude: 19.433333,
-                longitude: -99.133333
-            }
-        },
-        {
-            origin: {
-                latitude: ctry.au[0],
-                longitude: ctry.au[1]
-            },
-            destination: {
-                latitude: 9.933333,
-                longitude: -84.083333
-            }
-        },
-        {
-            origin: {
-                latitude: ctry.au[0],
-                longitude: ctry.au[1]
-            },
-            destination: {
-                latitude: 54.597 ,
-                longitude: -5.93
-            }
-        },
-        {
-            origin: {
-                latitude: ctry.au[0],
-                longitude: ctry.au[1]
-            },
-            destination: {
-                latitude: 52.516667,
-                longitude: 13.383333 
-            }
-        },
-        {
-            origin: {
-                latitude: ctry.au[0],
-                longitude: ctry.au[1]
-            },
-            destination: {
-                latitude: 14.692778,
-                longitude: -17.446667
-            }
-        },
-        {
-            origin: {
-                latitude: ctry.au[0],
-                longitude: ctry.au[1]
-            },
-            destination: {
-                latitude: -26.204444,
-                longitude: 28.045556
-            }
-        },
-                {
-            origin: {
-                latitude: ctry.au[0],
-                longitude: ctry.au[1]
-            },
-            destination: {
-                latitude: -6.8,
-                longitude: 39.283333 
-            }
-        },
-                {
-            origin: {
-                latitude: ctry.au[0],
-                longitude: ctry.au[1]
-            },
-            destination: {
-                latitude: 59.329444,
-                longitude: 18.068611
-            }
-        },
-                {
-            origin: {
-                latitude: ctry.au[0],
-                longitude: ctry.au[1]
-            },
-            destination: {
-                latitude: 59.95 ,
-                longitude: 30.3
-            }
-        }
-    ];
+    data: {
+      AUS: {
+        fillKey: 'LOW',
+        numberOfThings: 2002
+      },
+      USA: {
+        fillKey: 'MEDIUM',
+        numberOfThings: 10381
+      }
+    },
 
-  election.arc( presidentialTrips, {strokeWidth: 2});
+
+    geographyConfig: {
+      popupTemplate: function(geo, data) {
+          return ['<div>' + data.numberOfThings + '</div>'].join('');
+      }
+    }
+
+  });
+
+
+  var migrationArcs = [
+      {
+          origin: {
+              latitude: ctry.au[0],
+              longitude: ctry.au[1]
+          },
+          destination: {
+              latitude: 32.066667,
+              longitude: 34.783333 
+          }
+      },
+      {
+          origin: {
+              latitude: ctry.au[0],
+              longitude: ctry.au[1]
+          },
+          destination: {
+              latitude: 19.433333,
+              longitude: -99.133333
+          }
+      },
+      {
+          origin: {
+              latitude: ctry.au[0],
+              longitude: ctry.au[1]
+          },
+          destination: {
+              latitude: 9.933333,
+              longitude: -84.083333
+          }
+      },
+      {
+          origin: {
+              latitude: ctry.au[0],
+              longitude: ctry.au[1]
+          },
+          destination: {
+              latitude: 54.597 ,
+              longitude: -5.93
+          }
+      },
+      {
+          origin: {
+              latitude: ctry.au[0],
+              longitude: ctry.au[1]
+          },
+          destination: {
+              latitude: 52.516667,
+              longitude: 13.383333 
+          }
+      },
+      {
+          origin: {
+              latitude: ctry.au[0],
+              longitude: ctry.au[1]
+          },
+          destination: {
+              latitude: 14.692778,
+              longitude: -17.446667
+          }
+      },
+      {
+          origin: {
+              latitude: ctry.au[0],
+              longitude: ctry.au[1]
+          },
+          destination: {
+              latitude: -26.204444,
+              longitude: 28.045556
+          }
+      },
+              {
+          origin: {
+              latitude: ctry.au[0],
+              longitude: ctry.au[1]
+          },
+          destination: {
+              latitude: -6.8,
+              longitude: 39.283333 
+          }
+      },
+              {
+          origin: {
+              latitude: ctry.au[0],
+              longitude: ctry.au[1]
+          },
+          destination: {
+              latitude: 59.329444,
+              longitude: 18.068611
+          }
+      },
+              {
+          origin: {
+              latitude: ctry.au[0],
+              longitude: ctry.au[1]
+          },
+          destination: {
+              latitude: 59.95 ,
+              longitude: 30.3
+          }
+      }
+  ];
+
+  migrationMap.arc( migrationArcs, {strokeWidth: arcWidth});
 
 
 
